8861310e284d33b40658906c7d84d87383b2a4c6
Areej Omar Abumuhfouz <areejabumahfouz@gmail.com>
2024-06-04 16:21:02 +0300

    added by areej

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # loopTask
+#___________________________________________________________
+# this task about how i can loop and while at deference ways.
+## Looping is a fundamental concept in programming, allowing you to execute a block of code repeatedly. There are various ways to implement looping, with the two most common methods being the while loop and the for loop.
---
+++ app.js
@@ -0,0 +1,36 @@
+
+// Question 7
+
+// for(let i=0;i<=5;i++){
+//     window.alert(i);
+//     }
+
+
+// // Question 8
+// const num=[];
+// for(let i=0;i<=5;i++){
+//      num.push(i+" ");
+//         }
+// window.alert(num);
+    
+// Question 9
+// const num=[];
+// for(let i=0;i<=20;i+=3){
+//      num.push(i+" ");
+//         }
+// window.alert(num);
+
+// Question 10
+
+// let NuM=window.prompt("Enter number from 1 to 100","");
+// while(NuM<0 || NuM>100){
+//     window.alert("Error number you should enter number from 0 to 100!!!");
+// }
+
+// Question 11
+// let NuM=window.prompt("Enter number from 1 to 100","");
+// let n=0;
+// for(let i=0;i<=NuM;i++){
+//     n=n+i;
+// }
+// window.alert(n);
